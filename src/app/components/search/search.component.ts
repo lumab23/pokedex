be5b3fef4db174e22c5187290efec555f685bb7d
lumab23 <lumabca@gmail.com>
2025-03-19 18:29:36 -0300

    added the api

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule
+} from '@angular/forms';
+import { CardComponent } from '../card/card.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-search',
-  imports: [],
+  imports: [ReactiveFormsModule, CardComponent],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
 export class SearchComponent {
+
+  private http = inject(HttpClient);
+
+  showCard = false;
+  pokemonName = "";
+  sprite = "";
+
+  searchForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+  });
+
+  onSubmit() {
+    this.showCard = true; // show the pokemon card when the form is submitted
+    const name = this.searchForm.value.name;
+    console.log(name);
+    this.http
+      .get(`https://pokeapi.co/api/v2/pokemon/${name}`)
+      .subscribe((result: any) => {
+        console.log(result);
+        this.pokemonName = result.name;
+        this.sprite = result.sprites.front_default;
+      })
+  }
 
 }
